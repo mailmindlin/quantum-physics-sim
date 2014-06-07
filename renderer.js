c41f63d16d7ec824740f2c14e77f2d96851e49f4
mailmindlin <liamttlyrox@gmail.com>
2014-06-06 19:27:28 -0500

    Created renderer.js
    First commit!!

---
+++ renderer.js
@@ -0,0 +1,12 @@
+/*
+Renderer.js
+*/
+/**
+ * Creates a 'scene'. A scene contains all the atoms and renderers and stuff
+ * for a single simulation. Multiple scenes can be run at a time.
+ */
+window['Scene'] = function() {
+  var self = this;
+  
+  return self;
+}
