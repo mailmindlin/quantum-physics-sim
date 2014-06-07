a5187dea298f0aa7180b0225b314856e1800ca41
mailmindlin <liamttlyrox@gmail.com>
2014-06-06 20:06:45 -0500

    Added/fleshed out methods in Scene
    Added Scene.registry.*
    Added Scene.registerPhysicsHandler
    Added Scene.update
    TODO: finish Scene.update, add physics handlers.

--- renderer.js
+++ renderer.js
@@ -1,5 +1,6 @@
 /*
 Renderer.js
+Requires Utils.js amd Logger.js
 */
 /**
  * Creates a 'scene'. A scene contains all the atoms and renderers and stuff
@@ -7,6 +8,44 @@ Renderer.js
  */
 window['Scene'] = function() {
   var self = this;
-  
+  self.registry = {};
+  self.registry.physicsHandlers = new Array();
+  /**
+   * Registers a physics handler.
+   * @param handler: physics handler to register
+   * @param seniority: [Optional, default -1] order at which they are called (higher numbers called first)
+   * @see Scene#update(ticks)
+   */
+  self.registerPhysicsHandler = function(handler, seniority/*optional*/) {
+    self.registry.physicsHandlers.push({handler:handler, seniority:(ISSET(seniority)?seniority:-1));
+    return self;//allow for chaining
+  };
+  /**
+   * Updates the scene the specified number of ticks.
+   * @param ticks: [Optional, default 1] number of ticks to update.
+   * @return errorlevel: 0 is good, -1 or a thrown error is bad.
+   */
+  self.update = function(ticks) {
+  	var logger = Logger.create("renderer.js", "Scene#update");
+  	if (ISSET(ticks)) {
+  		if(ticks <= 0) {
+  			throw(new Error("Called Scene.update(ticks) with ticks=" + ticks + " (ticks must be greater than 0)."));
+  			return -1;//probably unreachable code
+  		}
+  		for(var i = 0; i < ticks; i++) {
+  			self.update();
+  		}
+  		return 0;
+  	} else {
+  		//order physicsHandlers
+  		var handlerArr = {};
+  		for(var handlerObj in self.registry.physicsHandlers) {
+  			if(typeof handlerArr[handlerObj['seniority']] !== 'object') handlerArr[handlerObj['seniority']] = [];
+  			handlerArr[handlerObj['seniority']].push(handlerObj['handler']);
+  		}
+  		//call physicshandlers, in order.
+  		//TODO finish
+  	}
+  }
   return self;
 }
