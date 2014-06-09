4914abab6caee620e6c2a0adba4f7598efa1dc56
mailmindlin <wfeehery17@17mbpro166.local>
2014-06-09 15:38:56 -0400

    Added help.js
    Basically, it uses the help-div to display help without popups

---
+++ help.js
@@ -0,0 +1,31 @@
+/*
+help.js
+*/
+var Help = {
+	logger: Logger.create("Help.js", "help");
+	register: function(domEl, helpText) {
+		$(domEl).attr('help-rq', 'true')
+			.attr('help-text', helpText);
+		Help.refresh();
+	},
+	unregister: function(element) {
+		$(element).attr('help-rq', 'false');
+	},
+	refresh: function() {
+		$('[help-rq="true"]').off('mouseover mouseout').unbind('mouseover mouseout')
+			.bind('mouseover', function(ev) {
+				Help.logger.log(ev);
+				Help.help(ev.currentTarget);
+			}).bind('mouseout', function(ev) {
+				Help.logger.log(ev);
+				if($(ev.currentTarget).attr('help-active')=='true') {
+					$('#help-div').hide();
+					$(ev.currentTarget).attr('help-active', false);
+				}
+			});
+	},
+	help: function(domEl) {
+		$('#help-div').show()
+			.html($(domEl).attr('help-text'));
+	}
+};
