28b303ae8ca3566f7bc5577779b44dc78893bd6c
mailmindlin <wfeehery17@17mbpro166.local>
2014-06-09 10:58:06 -0400

    Improved input.js
    Streamlined code, added stuff(i.e., finished self.addRow)

--- input.js
+++ input.js
@@ -6,7 +6,9 @@ function Input(args) {
 	if((!ISSET(args['name'])) && (!ISSET(args,'String')))throw(new Error("You need to specify name!"));
 	if(ISSET(args, 'String'))args['name']=args;
 	var self = this;
+	self.logger=new Logger('input.js', 'Input');
 	//constants
+	//table row
 	self.tRow="<tr>\
 	<td><input type='text' class='input-element input'/></td>\
 	<td><input type='text' class='input-X input'/></td>\
@@ -16,21 +18,24 @@ function Input(args) {
 	
 	self.name=args['name'];
 	self.addRow=function(){
-		if(ISSET(self.dom)){
-		
-		}else{
-			self.dom=$('table#'+self.name);
-			
+		if(!ISSET(self.dom)){
+			//get table element
+			self.dom=$('table#'+self.name)[0];
 		}
+		var tbody=$(self.dom).find("tbody");
+		tbody.append($(self.tRow));
+		self.logger.log('Successfully added row!');
 	};
 	if(ISSET(args['dom'])){
+		//create input
+		if(ISSET($('#'+args['name'])[0]))throw(new Error('Input with specified name \'' + args['name'] + '\' already exists'));
 		self.dom=dom;
 		dom.innerHTML="\
 			<table id="+args['name']+">\
 			<thead><tr><th>Element</th><th>X</th><th>Y</th><th>Z</th></tr></thead>\
 			<tbody></tbody>\
 			</table>";
-		
+		self.addRow();
 	}
 	return self;
 }
