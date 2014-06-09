600902fcb52e24a3ccad3c3b0d4b191101c906ee
mailmindlin <liamttlyrox@gmail.com>
2014-06-09 07:33:25 -0500

    Added function descriptions

--- utils.js
+++ utils.js
@@ -2,6 +2,11 @@
 Utils.js
 Standard utilities.
 */
+/**
+ * Determines whether variable a is:
+ * 1)set (if b is not used or is null)
+ * 2)the type specified by b (i.e., ISSET(null, 'undefined')==true)
+ */
 window['ISSET']=function(a,b) {
 	if(typeof b === 'undefined') {
 		return typeof a !== 'undefined';
@@ -11,9 +16,15 @@ window['ISSET']=function(a,b) {
 	throw(new Error("How did I get here?"));
 };
 //std. string functions
+/**
+ * Determines whether the string starts with the substring needle
+ */
 String.prototype.startsWith = function(needle) {
 	return this.indexOf(needle) == 0;
 };
+/**
+ * Determines whether the string contains the substring needle
+ */
 String.prototype.contains = function(needle) {
 	return this.indexOf(needle) >= 0;
 };
