e6383f5ad4defc51765cfc4d3181820461f38d4d
mailmindlin <liamttlyrox@gmail.com>
2014-06-09 23:23:39 -0400

    Made code awesomer

--- session.js
+++ session.js
@@ -3,19 +3,18 @@
 */
 var Session = (function() {
   var iData={};
-  var name="Session-1";
+  iData['session-name']="Session";
+  iData['autosave']=true;
   function update() {
-    
+    if(iData['autosave'])save();
   }
   return {
     set: function(k,v){iData[k]=v;update();},
     get: function(k){return iData[k];},
-    setName: function(n){
-      name=n
-    },
-    getName: function(){return name;},
+    setName: function(n){set('session-name',n);},
+    getName: function(){return get('session-name');},
     getAll: function(){return iData;},
-    load: function(n) {iData=JSON.parse(window.localStorage.getItem(n));name=n;},
-    save: function(n) {window.localStorage.setItem((typeof n !== 'undefined')?n:name, JSON.stringify(iData));}
+    load: function(n) {iData=JSON.parse(window.localStorage.getItem(n));set('session-name',n);},
+    save: function(n) {window.localStorage.setItem((typeof n !== 'undefined')?n:get('session-name'), JSON.stringify(iData));}
   };
 })();
