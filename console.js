e031e461f3e81b652557d4b5e75dc4979d116a31
mailmindlin <liamttlyrox@gmail.com>
2014-06-10 13:17:27 -0400

    Update console.js

--- console.js
+++ console.js
@@ -5,11 +5,20 @@ allow scripting
 var Console = function(data){
 	var self = this;
 	self.data = data;
-	self.exec = function(){
+	self.exec = function() {
 		var lines = data.split('\n');
+		var output="";
 		for(var i=0;i<lines.length;i++){
 			var line = lines[i];
-			
+			var out=self.execLn();
+			output=line+"\n\t"+(out!==null?out:"undefined")+"\n";
 		}
+		return output;
 	};
+	self.execLn = function(ln) {
+		if(ln.startsWith('DEFINE')){
+			ln=ln.substring(7);//remove 'DEFINE '
+			//do stuff
+		}
+	}
 };
