e7d36d60761d4bd03e4e21199b588d66d9fada4b
mailmindlin <liamttlyrox@gmail.com>
2014-06-10 11:22:42 -0400

    Integrated css3 animations

--- demo/main.js
+++ demo/main.js
@@ -2,11 +2,11 @@ var isFooterVisible=true;
 function toggleFooter(){
 	if(isFooterVisible==true){
 		$('#footer-wrapper').slideUp();
-		$('#btm-arrow').animate({top:$('#footer-wrapper').height()});
+		$('#btm-arrow').animate({top:$('#footer-wrapper').height()}).addClass('active').removeClass('inactive');
 		isFooterVisible=false;
 	}else{
 		$('#footer-wrapper').slideDown();
-		$('#btm-arrow').animate({top:-$('#footer-wrapper').height()});
+		$('#btm-arrow').animate({top:-$('#footer-wrapper').height()}).addClass('inactive').removeClass('active');
 		isFooterVisible=true;
 	}
 }
