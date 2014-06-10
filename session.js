e1b0978d9887eab2838614d859f6c1971d9efe05
mailmindlin <liamttlyrox@gmail.com>
2014-06-09 23:28:18 -0400

    Fixed syntax error

--- session.js
+++ session.js
@@ -11,10 +11,10 @@ var Session = (function() {
   return {
     set: function(k,v){iData[k]=v;update();},
     get: function(k){return iData[k];},
-    setName: function(n){set('session-name',n);},
-    getName: function(){return get('session-name');},
+    setName: function(n){iData['session-name']=n;},
+    getName: function(){return iData['session-name'];},
     getAll: function(){return iData;},
-    load: function(n) {iData=JSON.parse(window.localStorage.getItem(n));set('session-name',n);},
-    save: function(n) {window.localStorage.setItem((typeof n !== 'undefined')?n:get('session-name'), JSON.stringify(iData));}
+    load: function(n) {iData=JSON.parse(window.localStorage.getItem(n));iData['session-name']=n;},
+    save: function(n) {window.localStorage.setItem((typeof n !== 'undefined')?n:iData['session-name']), JSON.stringify(iData));}
   };
 })();
