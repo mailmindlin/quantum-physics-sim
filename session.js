2f5e7beb0c04dab3ee21cfb37805f6c218fe48b7
mailmindlin <liamttlyrox@gmail.com>
2014-06-09 23:13:13 -0400

    Fixed storage glitch

--- session.js
+++ session.js
@@ -3,14 +3,19 @@
 */
 var Session = (function() {
   var iData={};
+  var name="Session-1";
   function update() {
     
   }
   return {
     set: function(k,v){iData[k]=v;update();},
     get: function(k){return iData[k];},
+    setName: function(n){
+      name=n
+    },
+    getName: function(){return name;},
     getAll: function(){return iData;},
-    load: function(name) {iData=window.localStorage.getItem(name);},
-    save: function(name) {window.localStorage.setItem(name, iData);}
+    load: function(n) {iData=JSON.parse(window.localStorage.getItem(n));name=n;},
+    save: function(n) {window.localStorage.setItem((typeof n !== undefined)?n:name, JSON.stringify(iData));}
   };
 })();
