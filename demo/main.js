bb8eb51d24a1d74324475f8fce1c873c08dab482
mailmindlin <liamttlyrox@gmail.com>
2014-06-10 10:45:57 -0400

    Added footer control function

--- demo/main.js
+++ demo/main.js
@@ -1,3 +1,14 @@
-function displayBottomArrow(show){
-	
+var isFooterVisible=true;
+function toggleFooter(){
+	if(isFooterVisible=true){
+		$('#footer-wrapper').slideUp();
+		isFooterVisible==false;
+	}else{
+		$('#footer-wrapper').slideUp();
+		isFooterVisible==true;
+	}
 }
+$('body').ready(function(){
+	toggleFooter();
+	$('#btm-arrow').click(toggleFooter());
+});
