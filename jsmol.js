549d104f88367f34e497b59c4d56d2fce642d6c5
mailmindlin <client>
2014-06-13 13:34:12 -0400

    Added object descriptions
    added Atom, xyzFile, and scene.

--- jsmol.js
+++ jsmol.js
@@ -14,4 +14,37 @@ var JSmolInterface = {
 	start: function(domEl){
 		$(domEl).html(Jmol.getAppletHtml("JSmol1",JSmolInterface.info));
 	}
+	atom: function(type,x,y,z,charge,vectorX,vectorY,vectorZ) {
+		var self = new Object();
+		self.atomType = type;
+		self.x=x;
+		self.y=y;
+		self.z=z;
+		self.charge=charge;
+		self.vectorX=vectorX;
+		self.vectorY=vectorY;
+		self.vectorZ=vectorZ;
+		self.type="Atom";
+		return self;
+	},
+	xyzFile: function() {
+		var self = new Object();
+		self.atomRegistry = [];
+		self.pushAtom = function(atom) {
+			//make sure atom is an atom
+			if(atom.type!="Atom")throw(new Error("argument[0] was not an atom!"));
+			self.atomRegistry.push(atom);
+		};
+		self.getXYZData = function(comment) {
+			var output = atomRegistry.length + "\n";
+			output +=comment;
+			
+		}
+		return self;
+	},
+	scene: function() {
+		var self = this;
+		
+		return self;
+	}
 };
