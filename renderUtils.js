24221067781786f3784a3b8e738f4ca7faaa985e
mailmindlin <liamttlyrox@gmail.com>
2014-06-14 22:25:16 -0500

    Added awesomeness

--- renderUtils.js
+++ renderUtils.js
@@ -1,9 +1,15 @@
 window['SceneRenderer'] = function(g3, scene) {
-
+  var self = this;
+  
+  return self;
 };
 window['SphereRenderer'] = function(g3, x,y,z,r) {
-
+  var self = this;
+  
+  return self;
 };
 window['CylinderRenderer'] = function(g3, x, y, z, r, h) {
+  var self = this;
   
+  return self;
 };
